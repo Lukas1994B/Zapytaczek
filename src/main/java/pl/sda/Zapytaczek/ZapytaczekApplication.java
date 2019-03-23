17149a4326dbf183e3878c9bebb2dec6ee9e343f
Łukasz Bresz <breszlukasz@gmail.com>
2019-03-23 11:44:38 +0100

    first commit

---
+++ src/main/java/pl/sda/Zapytaczek/ZapytaczekApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.Zapytaczek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapytaczekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapytaczekApplication.class, args);
+	}
+
+}
